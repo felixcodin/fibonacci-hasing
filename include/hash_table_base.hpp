1872ad0bd42eb978a633afbdfb32a48c98019018
Nguyen Nguyen <brightsmileslikesunshine@gmail.com>
2025-06-16 13:18:40 +0700

    Update hash_table_base.hpp file

--- include/hash_table_base.hpp
+++ include/hash_table_base.hpp
@@ -10,6 +10,12 @@ class HashTableBase
         virtual void insert(const string &key, int value) = 0;
         virtual bool get(const string &key, int &value) = 0;
         virtual void remove(const string &key) = 0;
+
+        virtual int getElementCount() const = 0;
+        virtual double getLoadFactor() const = 0;
+        virtual double getAverageChainLength() const = 0;
+        virtual int getMaxChainLength() const = 0;
+
         virtual ~HashTableBase() = default;
 };
 
